77975dc4755a748613283683b05c2f413b9de135
刘怀轩 <liuhuaixuan@>
2017-05-10 17:29:07 +0800

    Initial Commit

---
+++ WebHeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebHeight
+//
+//  Created by 刘怀轩 on 2017/5/10.
+//  Copyright © 2017年 刘怀轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebHeightUITests/WebHeightUITests.m
@@ -0,0 +1,40 @@
+//
+//  WebHeightUITests.m
+//  WebHeightUITests
+//
+//  Created by 刘怀轩 on 2017/5/10.
+//  Copyright © 2017年 刘怀轩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebHeightUITests : XCTestCase
+
+@end
+
+@implementation WebHeightUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
